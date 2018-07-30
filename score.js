ff324479c6be0bd6241db5be1671b5b85b3e5664
Brittney <brittfort34@gmail.com>
2018-07-30 17:23:35 +0000

    So far

---
+++ score.js
@@ -0,0 +1,15 @@
+var scores= [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 0, 1, 7, 3, 6, 4, 10, 0, 2, 8, 6];
+
+
+// A loop of some kind is obviously ideal for this, we are still working on that...
+var frame1 = scores.splice(0, 2);
+var frame2 = scores.splice(0, 2);
+var frame3 = scores.splice(0, 2);
+var frame4 = scores.splice(0, 2);
+var frame5 = scores.splice(0, 2);
+var frame6 = scores.splice(0, 2);
+var frame7 = scores.splice(0, 2);
+var frame8 = scores.splice(0, 2);
+var frame9 = scores.splice(0, 2);
+var frame10 = scores.splice(0, 3);
+
